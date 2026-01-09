bcb2c0851f3a3a9a6cb7162a3bd72f8847443018
lanhlanh25 <dothilananh2005@gmail.com>
2026-01-09 20:37:43 +0700

    Khung

---
+++ Mobile_warehouse/src/java/dal/DBContext.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBContext {
+// Đổi tên DB, user, pass cho đúng của bạn
+
+    private static final String URL
+            = "jdbc:mysql://localhost:3306/mobile_warehouse"
+            + "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+
+    private static final String USER = "root";
+    private static final String PASS = "123456";
+
+    public static Connection getConnection() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(URL, USER, PASS);
+    }
+}
